26be698dc2a14b5474debb06830c56867d02df7e
MforMad <smb.like.you@gmail.com>
2016-04-29 01:53:07 +0600

    Add declaration of clear_table func

--- src/code.c
+++ src/code.c
@@ -8,6 +8,7 @@ unsigned int neighbor_count(char table[T_WIDTH][T_HEIGHT], unsigned int height, 
 unsigned int endgame_check(char past_table[T_WIDTH][T_HEIGHT], char future_table[T_WIDTH][T_HEIGHT]);
 unsigned int next_gen(char past_table[T_WIDTH][T_HEIGHT]);
 void print_table(char table[T_WIDTH][T_HEIGHT]);
+void clear_table(char table[T_WIDTH][T_HEIGHT]);
 
 int main()
 {
