9321afb1aa3b933ddc3b9b35542e5ad43d745e7c
MforMad <smb.like.you@gmail.com>
2016-05-02 23:37:40 +0600

    Add example of reading the first gen

--- src/code.c
+++ src/code.c
@@ -22,7 +22,13 @@ int main()
 	char table[T_WIDTH][T_HEIGHT];
 
 	clear_table(table);
+/*
+	FILE *in = fopen("/home/dmitriy/glife/game-of-life/src/test.txt", "r+");
 
+	read_table(table, in);
+
+	fclose(in);
+*/
 	first_gen(table);
 
 	system("resize -s 10 20");
