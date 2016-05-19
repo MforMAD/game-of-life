ac0baf0abae38fc868a1c7a050500a631fd9459c
Detect1ve <detect1ve@ubuntu.ubuntu-domain>
2016-05-20 05:55:12 +0800

    Creating a reader logic

--- src/game.c
+++ src/game.c
@@ -195,6 +195,18 @@ int main_menu()
 {
 	char ch;
 	printf("1.Новая игра\n2.Помощь\n3.Выход\n");
-	scanf("%s",ch); 
-	return 0;
+	scanf("%s", &ch);
+	switch (ch)
+	{
+		case 1:
+			return 1;
+			break;
+		case 2:
+			return 0;
+			break;
+		case 3:
+			return -1;
+			break;
+	}
+	return -1;
 }
