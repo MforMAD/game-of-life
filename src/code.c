17f7b028e0b4bb759efc4a9bf741bb2bf123c6cb
Evgerher <evgerher1l@gmail.com>
2016-05-31 22:22:37 +0600

    Check the existence of GTK

--- src/code.c
+++ src/code.c
@@ -6,34 +6,37 @@
 
 int main()
 {
+	int GTK = 0; //We check, Does this machine have the GTK library. If true we run interface, else we show console version
 	srand(time(NULL));
 	char table[T_WIDTH][T_HEIGHT];
-	int status;
 
-	status = main_menu();
-	if (status == 1) {
-		clear_table(table);
-/*
-	FILE *in = fopen("/home/dmitriy/glife/game-of-life/src/test.txt", "r+");
+	if (GTK) {
 
-	read_table(table, in);
-
-	fclose(in);
-*/
-		first_gen(table);
-
-		system("resize -s 10 20");
-		system("clear");
-
-		do {
-			print_table(table);
-			usleep(1000 * 100);
-		} while (!next_gen(table));
-
-		print_table(table);
+		return 0;
 	}
 
-	if (status == -1)
+	else {
+		int status;
+
+		status = main_menu();
+		if (status == 1) {
+			clear_table(table);
+			first_gen(table);
+
+			system("resize -s 10 20");
+			system("clear");
+
+			do {
+				print_table(table);
+				usleep(1000 * 100);
+			} while (!next_gen(table));
+
+			print_table(table);
+		}
+
+		if (status == -1)
+			return 0;
 		return 0;
-	return 0;
+	}
+
 }
