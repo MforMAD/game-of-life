012ec54add65570126a2fd85ae27855b1195efc2
Evgerher <evgerher1l@gmail.com>
2016-06-01 00:02:03 +0600

    Create boxes, decorate the window

--- src/code.c
+++ src/code.c
@@ -13,13 +13,12 @@ int main(int argc, char *argv[])
 
 	if (GTK) {
 		int status;
-		GtkApplication *app;
 		struct base basement;
 
-		app = gtk_application_new("game.of.life", G_APPLICATION_FLAGS_NONE);
-		g_signal_connect(app, "activate", G_CALLBACK(start), NULL);
-		status = g_application_run(G_APPLICATION(app), argc, argv);
-		g_object_unref(app);
+		basement.app = gtk_application_new("game.of.life", G_APPLICATION_FLAGS_NONE);
+		g_signal_connect(basement.app, "activate", G_CALLBACK(start), &basement);
+		status = g_application_run(G_APPLICATION(basement.app), argc, argv);
+		g_object_unref(basement.app);
 
 		return status;;
 	}
--- src/interface.c
+++ src/interface.c
@@ -2,5 +2,20 @@
 
 void start(GtkApplication *app, gpointer data)
 {
-	g_print("Start\n");
+	struct base *Base = (struct base *)data;
+	struct box *Box = &(basement->boxes);
+	struct tpl *Tpl = &(basement->templates);
+
+	Base->window = gtk_application_window_new(Base->app);
+	gtk_window_set_title(GTK_WINDOW(Base->window), "Game of life");
+	gtk_container_set_border_width(GTK_CONTAINER(Base->window), 50);
+	
+	Box->content = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+	Box->menu = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+	Box->rules = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+	Box->levels = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+	Box->game = gtk_grid_new();
+
+	gtk_container_add(GTK_CONTAINER(Base->window), Box->content);
+	gtk_widget_show_all(Base->window);
 }
