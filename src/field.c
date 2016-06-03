545bb806018680d8f9a8f34432fdfbfd7ab77f1c
MforMad <smb.like.you@gmail.com>
2016-06-04 02:22:23 +0600

    Declare field_neighbors_count func

--- src/field.c
+++ src/field.c
@@ -1,3 +1,4 @@
 #include <field.h>
 
 static unsigned int field_endgame_check(field *temp_field);
+static unsigned int field_neighbors_count(field *temp_field, unsigned int h_pos, unsigned int w_pos);
