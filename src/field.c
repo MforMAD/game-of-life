7bb67eb1289b99420b2aa21a9dd6652e06e84ef8
MforMad <smb.like.you@gmail.com>
2016-06-04 02:22:42 +0600

    Declare field_table_delete func

--- src/field.c
+++ src/field.c
@@ -2,3 +2,4 @@
 
 static unsigned int field_endgame_check(field *temp_field);
 static unsigned int field_neighbors_count(field *temp_field, unsigned int h_pos, unsigned int w_pos);
+static void table_delete(char **temp_table, unsigned int table_size);
