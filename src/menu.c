3fa5f6472acf6b7348f411dc7afd648d0ff587b1
Detect1ve <detect1ve@ubuntu.ubuntu-domain>
2016-06-04 14:55:03 +0800

    Import sourse code from game.c to menu.c

--- src/menu.c
+++ src/menu.c
@@ -1,1 +1,21 @@
+int main_menu()
+{
+        char ch;
+        printf("1.Новая игра\n2.Помощь\n3.Выход\n");
+        scanf("%c", &ch);
+        switch (ch)
+        {
+                case '1':
+                        return 1;
+                        break;
+                case '2':
+                        return 0;
+                        break;
+                case '3':
+                        return -1;
+                        break;
+        }
+        return -1;
+}
+
 
