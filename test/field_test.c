1bcec406c0052542765e0bce4fdb95f580182824
MforMad <smb.like.you@gmail.com>
2016-06-04 12:22:17 +0600

    Add next_gen_nonstatic_test

--- test/field_test.c
+++ test/field_test.c
@@ -37,7 +37,7 @@ CTEST(endgame_check_suite, dead_field_test) {
     table_set(test_field, test_table, 1);
 
     // When
-    unsigned int result = field_endgame_check(test_field);
+    const unsigned int result = field_endgame_check(test_field);
     field_delete(test_field);
     table_delete(test_table, 4);
 
@@ -476,4 +476,82 @@ CTEST(neighbos_count_suite, tor_bottom_right_corner_field_test) {
     const unsigned int expected_result = 3;
 
     ASSERT_EQUAL(expected_result, result);
+}
+
+CTEST(next_gen_suite, next_gen_test) {
+    // Given
+    unsigned int i;
+    unsigned int j;
+
+    field *test_field;
+
+    char **test_table = malloc(4 * sizeof(char*));
+
+    for (i = 0; i < 4; i++) {
+        test_table[i] = malloc(4 * sizeof(char));
+    }
+
+    test_field = field_create(4);
+
+    {
+        test_table[0][0] = 0;
+        test_table[0][1] = 1;
+        test_table[0][2] = 0;
+        test_table[0][3] = 0;
+        test_table[1][0] = 0;
+        test_table[1][1] = 1;
+        test_table[1][2] = 0;
+        test_table[1][3] = 0;
+        test_table[2][0] = 0;
+        test_table[2][1] = 1;
+        test_table[2][2] = 0;
+        test_table[2][3] = 0;
+        test_table[3][0] = 0;
+        test_table[3][1] = 0;
+        test_table[3][2] = 0;
+        test_table[3][3] = 0;
+    }
+
+    table_set(test_field, test_table, 0);
+
+    {
+        test_table[0][0] = 0;
+        test_table[0][1] = 0;
+        test_table[0][2] = 0;
+        test_table[0][3] = 0;
+        test_table[1][0] = 1;
+        test_table[1][1] = 1;
+        test_table[1][2] = 1;
+        test_table[1][3] = 0;
+        test_table[2][0] = 0;
+        test_table[2][1] = 0;
+        test_table[2][2] = 0;
+        test_table[2][3] = 0;
+        test_table[3][0] = 0;
+        test_table[3][1] = 0;
+        test_table[3][2] = 0;
+        test_table[3][3] = 0;
+    }
+
+    // When
+    const unsigned int result = field_next_gen(test_field);
+    unsigned int diff = 0;
+
+    for (i = 0; i < 4; i++){
+        for (j = 0; j < 4; j++) {
+            if (test_field->current_table[i][j] != test_table[i][j]) {
+                diff++;
+            }
+        }
+    }
+
+    field_delete(test_field);
+    table_delete(test_table, 4);
+
+    // Then
+    const unsigned int expected_result = 0;
+    const unsigned int expected_diff = 0;
+
+    ASSERT_EQUAL(expected_result, result);
+    ASSERT_EQUAL(expected_diff, diff);
 }
